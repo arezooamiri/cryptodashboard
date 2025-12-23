648c1fd56f723cae6d54dcaeff506e8ac3ab910b
Arezoo <arezooayoblo@gmail.com>
2025-12-23 10:36:41 +0330

    localstorage

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,34 +1,46 @@
+import { create } from "zustand";
+import { persist, createJSONStorage  } from "zustand/middleware";
 
-import {create} from 'zustand'
 
-type Coin={
-    image:string;
-    id:string;
-    symbol:string;
-    current_price:number;
-    market_cap_change_percentage_24h:number;
-}
+type Coin = {
+  image: string;
+  id: string;
+  symbol: string;
+  current_price: number;
+  market_cap_change_percentage_24h: number;
+};
 
-type CoinStore={
-    
-    selectCoins:Coin[];
-    addCoin:(coins:Coin)=>void;
-}
-type Activation={
-    isActive:boolean;
-    toggleActive:()=>void;
-    setIsActive:(value:boolean)=>void
-}
-export const selectCoin=create<CoinStore>((set)=>({
-      selectCoins:[],
-      addCoin:(coin)=> set((state)=>(
-{selectCoins:[...state.selectCoins,coin]}))
-}))
+type CoinStore = {
+  selectCoins: Coin[];
+  addCoin: (coins: Coin) => void;
+};
+type Activation = {
+  isActive: boolean;
+  toggleActive: () => void;
+  setIsActive: (value: boolean) => void;
+};
+export const selectCoin = create<CoinStore>()(
+    persist((set) => ({
+    selectCoins: [] as Coin[],
+   
+    addCoin: (coin) =>
+       
+      set((state) => {
+        const exist=state.selectCoins.some(c=>c.id===coin.id)  ;
+        if(exist){
+            return state;
+        }else{
+                return { selectCoins: [...state.selectCoins, coin] }
+        }
+  
+      }
+    )
+        
+  }),{name:"coin-storage",storage:createJSONStorage(()=>sessionStorage)})
+);
 
-export const buttonAactive=create<Activation>((set)=>(
-    {
-        isActive:true,
-       toggleActive:()=>set((state)=>({isActive:!state.isActive})),
-        setIsActive:(value)=>set({isActive:value})
-    }
-))
+export const buttonAactive = create<Activation>((set) => ({
+  isActive: true,
+  toggleActive: () => set((state) => ({ isActive: !state.isActive })),
+  setIsActive: (value) => set({ isActive: value }),
+}));
